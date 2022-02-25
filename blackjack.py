3770470b1423e6f4825fa1eea4d4ca3bca163b4d
Tristan Sim <93330584+ssim3@users.noreply.github.com>
2022-02-25 19:48:56 +0800

    Added some shit

--- blackjack.py
+++ blackjack.py
@@ -14,6 +14,9 @@ class Player:
     def bet(self, bet_value):
         self.cash -= bet_value
 
+    def win(self, winnings):
+        self.cash += winnings
+
 
 def intro():
 
@@ -42,6 +45,52 @@ a88aaaa8P'  Y8aa8P     88    88aaaaa88a 88     88 88     88    a88aaaa8P' 88    
 
     for i in players:
         print(i) 
+    
+    return players
 
+def game(player_list):
+
+    cards = {"Ace of Clubs": 1, "Ace of Diamonds": 1, "Ace of Hearts": 1, "Ace of Spades": 1, 
+    "2 of Clubs": 2, "2 of Diamonds": 2, "2 of Hearts": 2, "2 of Spades": 2,
+    "3 of Clubs": 3, "3 of Diamonds": 3, "3 of Hearts": 3, "3 of Spades": 3,
+    "4 of Clubs": 4, "4 of Diamonds": 4, "4 of Hearts": 4, "4 of Spades": 4,
+    "5 of Clubs": 5, "5 of Diamonds": 5, "5 of Hearts": 5, "5 of Spades": 5,
+    "6 of Clubs": 6, "6 of Diamonds": 6, "6 of Hearts": 6, "6 of Spades": 6,
+    "7 of Clubs": 7, "7 of Diamonds": 7, "7 of Hearts": 7, "7 of Spades": 7,
+    "8 of Clubs": 8, "8 of Diamonds": 8, "8 of Hearts": 8, "8 of Spades": 8,
+    "9 of Clubs": 9, "9 of Diamonds": 9, "9 of Hearts": 9, "9 of Spades": 9,
+    "10 of Clubs": 10, "10 of Diamonds": 10, "10 of Hearts": 10, "10 of Spades": 10,
+    "King of Clubs": 10, "King of Diamonds": 10, "King of Hearts": 10, "King of Spades": 10,
+    "Queen of Clubs": 10, "Queen of Diamonds": 10, "Queen of Hearts": 10, "Queen of Spades": 10,
+    "Jack of Clubs": 10, "Jack of Diamonds": 10, "Jack of Hearts": 10, "Jack of Spades": 10}
+
+    dealer = Player("Dealer", 1000000000)
+    bet = int(input("\nEnter your bet (Maximum 100000): "))
+    
+    print("\n DEALER WILL NOW DEAL OUT CARDS")
+
+    # SUBTRACTING THE BETS FROM EACH PLAYER
+    for i in player_list:
+        # Makes sure player has enough money
+        if (i.cash > bet):
+            i.bet(bet)          
+            print(i.cash)
+        else:
+            print("{} is too broke to bet, removed from game.".format(i.name))
+            player_list.remove(i)
+
+    print(" ------------------------------------- ")
+    print("\n CARDS WILL NOW BE DRAWN!")
+
+    player_cards = []
+    
 
     
+
+        
+    
+
+
+
+players = intro()
+game(players)
